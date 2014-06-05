d0f06b79e62423b238a390858342febc0aeaada6
moaa <r.low.in.mt@gmail.com>
2014-06-05 15:22:21 -0600

    fixed issue where channel filtering was triggered on all channel events

--- lib/websocket_rails/dispatcher.rb
+++ lib/websocket_rails/dispatcher.rb
@@ -52,5 +52,76 @@ module WebsocketRails
       end
     end
 
+    private
+
+    def route(event)
+      actions = []
+      event_map.routes_for event do |controller_class, method|
+        actions << Fiber.new do
+          begin
+            log_event(event) do
+              controller = controller_factory.new_for_event(event, controller_class, method)
+
+              controller.process_action(method, event)
+            end
+          rescue Exception => ex
+            event.success = false
+            event.data = extract_exception_data ex
+            event.trigger
+          end
+        end
+      end
+      execute actions
+    end
+
+    def filter_channel(event)
+      actions = []
+      actions << Fiber.new do
+        begin
+          log_event(event) do
+            controller_class, catch_all = filtered_channels[event.channel]
+
+            controller = controller_factory.new_for_event(event, controller_class, event.name)
+            # send to the method of the event name
+            # silently skip routing to the controller on event.name if it doesnt respond
+            controller.process_action(event.name, event) if controller.respond_to?(event.name)
+            # send to our defined catch all method
+            controller.process_action(catch_all, event) if catch_all
+
+          end
+        rescue Exception => ex
+          event.success = false
+          event.data = extract_exception_data ex
+          event.trigger
+        end
+      end if filtered_channels[event.channel]
+
+      actions << Fiber.new{ WebsocketRails[event.channel].trigger_event(event) }
+      execute actions
+    end
+
+    def execute(actions)
+      actions.map do |action|
+        EM.next_tick { action.resume }
+      end
+    end
+
+    def extract_exception_data(ex)
+      if record_invalid_defined? and ex.is_a? ActiveRecord::RecordInvalid
+        {
+          :record => ex.record.attributes,
+          :errors => ex.record.errors,
+          :full_messages => ex.record.errors.full_messages
+        }
+      else
+        ex if ex.respond_to?(:to_json)
+      end
+    end
+
+    def record_invalid_defined?
+      Object.const_defined?('ActiveRecord') and ActiveRecord.const_defined?('RecordInvalid')
+    end
+
+
   end
 end
